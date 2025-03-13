4f2cc886d540f5d2c0b065d50c825118d751bc8f
aarushgupta05 <aarushgupta050510@gmail.com>
2025-03-13 19:12:21 +0800

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -45,7 +45,7 @@ st.markdown(
 def homepage():
     col1,col2=st.columns([0.8, 0.2])
     with col2:
-        st.image("logo.png", width=250)
+        st.image("logo.png", width=400)
     st.markdown('<h1 class="heading1">StemFlow</h1>', unsafe_allow_html=True)
     st.markdown('<h2 class="bodytext">StemFlow is a high-level machine learning, source separation based model that uses deep neural networks to split musical compositions into individual track stems. Go to "about StemFlow" for more information. </t>', unsafe_allow_html=True)
 def about_stemflow():
